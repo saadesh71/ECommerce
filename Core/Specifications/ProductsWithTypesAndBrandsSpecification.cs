8241f5dcdc21cb969eceee41958f2eef85f98a56
@askf2 <askf2@mst.edu>
2024-02-17 18:47:49 -0600

    Added Generics, Automapper, Debugger, Product images and URl

---
+++ Core/Specifications/ProductsWithTypesAndBrandsSpecification.cs
@@ -0,0 +1,22 @@
+using System;
+using System.Linq.Expressions;
+using System.Security.Cryptography.X509Certificates;
+using Core.Entities;
+
+namespace Core.Specifications
+{
+    public class ProductsWithTypesAndBrandsSpecification : BaseSpecification<Product>
+    {
+        public ProductsWithTypesAndBrandsSpecification()
+        {
+            AddInclude(x => x.ProductType);
+            AddInclude(x => x.ProductBrand);
+        }
+
+        public ProductsWithTypesAndBrandsSpecification(int id) : base(x => x.Id == id)
+        {
+            AddInclude(x => x.ProductType);
+            AddInclude(x => x.ProductBrand);
+        }
+    }
+}
